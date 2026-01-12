ca5bed7df4f990bf602c09b18de0c79ebb5724ef
Dylan Dobbs <ddesign.ddevelopment@gmail.com>
2026-01-12 09:37:52 -0500

    removed

--- recipes/app.py
+++ recipes/app.py
@@ -19,8 +19,8 @@ RECIPE_SCRAPER_URL = os.getenv("RECIPE_SCRAPER_URL", "http://localhost:8010").rs
 
 DB_HOST = os.getenv("DB_HOST", "postgres")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "homelab")
-DB_USER = os.getenv("DB_USER", "homelab")
+DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER", "")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
 if not DB_PASSWORD:
